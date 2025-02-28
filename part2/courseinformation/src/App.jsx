44891199a2c992a1a3885614484af4acb4acc06f
dustinburda <dustin99wii@gmail.com>
2025-02-27 19:15:33 -0500

    Finished 2.1, 2.2

---
+++ part2/courseinformation/src/App.jsx
@@ -0,0 +1,69 @@
+const Part = ({part}) => {
+  return (
+    <li>
+      {part.name} {part.exercises}
+    </li>
+  );
+}
+
+const Content = ({parts}) => {
+  return (
+    <ul>
+      {parts.map((part) => {
+        return (<Part part={part} key={part.id}/>);
+      })}
+    </ul>
+  );
+}
+
+
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  );
+}
+
+const Sum = ({parts}) => {
+  return (
+    <div style={{fontWeight: "bold"}}> total of {parts.reduce((accum, curr) => accum + curr.exercises, 0)} exercises</div>
+  );
+}
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header text={course.name}/>
+      <Content parts={course.parts}/>
+      <Sum parts={course.parts}/>
+    </>
+  );
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
