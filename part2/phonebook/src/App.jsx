11bcb3f661e5097dc5fd5b6cb749d4f17a99e7a7
dustinburda <dustin99wii@gmail.com>
2025-02-28 21:15:56 -0500

    Finished 2.9, 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,63 +1,93 @@
 import { useState } from 'react'
 
+
+const Form = ({persons, setPersons}) => {
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  return (
+    <form onSubmit={(event) => {
+      event.preventDefault();
+
+      if (persons.findIndex(person => person.name === newName) != -1 ) {
+        alert(`${newName} is already in the phonebook!`);
+        return;
+      }
+
+      if (newName === "") {
+        alert(`Please provide a name!`);
+        return;
+      }
+
+      if (newNumber === "") {
+        alert(`Please provide a phone number!`);
+        return;
+      }
+
+      const newPerson = {
+        name: newName,
+        phoneNumber: newNumber
+      }
+
+      setPersons([newPerson, ...persons]);
+    }}>
+      <div>
+        name: <input type="text" value={newName} onChange={(event) => {
+          const name = event.target.value;
+          setNewName(name);
+        }}/>
+      </div>
+      <div>
+        number: <input type="text" value={newNumber} onChange={event => {
+          const number = event.target.value;
+          setNewNumber(number);
+        }}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+}
+
+const Person = ({person}) => {
+  return (
+    <li key={person.name}>{person.name} {person.phoneNumber}</li>
+  );
+}
+
+const PhoneBook = ({persons}) => {
+  return (
+    <ul>
+      {persons.map(person => <Person person={person}/>)}
+    </ul>
+  );
+}
+
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
       phoneNumber: "040-1234567"
     }
   ]) 
+  const [filterValue, setFilterValue] = useState("")
 
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
+  let renderedPersons = [...persons];
+  if(filterValue != ""){
+    renderedPersons = persons.filter((person) => person.name.toLowerCase().includes(filterValue.toLowerCase()))
+  }
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-
-        if (persons.findIndex(person => person.name === newName) != -1 ) {
-          alert(`${newName} is already in the phonebook!`);
-          return;
-        }
-
-        if (newName === "") {
-          alert(`Please provide a name!`);
-          return;
-        }
-
-        if (newNumber === "") {
-          alert(`Please provide a phone number!`);
-          return;
-        }
-
-        const newPerson = {
-          name: newName,
-          phoneNumber: newNumber
-        }
-
-        setPersons([newPerson, ...persons]);
-      }}>
-        <div>
-          name: <input type="text" value={newName} onChange={(event) => {
-            const name = event.target.value;
-            setNewName(name);
-          }}/>
-        </div>
-        <div>
-          number: <input type="text" value={newNumber} onChange={event => {
-            const number = event.target.value;
-            setNewNumber(number);
-          }}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <div>
+        filter shown with <input value={filterValue} onChange={event => setFilterValue(event.target.value)} />
+      </div>
+      <Form persons={persons} setPersons={setPersons}/>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.phoneNumber}</li>)}
-      </ul>
+      <PhoneBook persons={renderedPersons}/>
     </div>
   )
 }
